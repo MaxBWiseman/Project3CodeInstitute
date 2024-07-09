a2a2cbb4ffdbb01a139b6cfef0346bf662234719
Max Wiseman <maxwise70@hotmail.co.uk>
2024-07-09 21:34:40 +0000

    chore: Update welcome function to display workout options from Google Sheet and ask the user for input to select desired muslce group

--- run.py
+++ run.py
@@ -23,13 +23,31 @@ SHOULDERS = WORKOUTS.col_values(4)
 LEGS = WORKOUTS.col_values(5)
 
 def welcome():
-   #Grab the workout names from the google sheet
-   workout_names = WORKOUTS.row_values(1)
-   print("Welcome to the Python Fitness Console!")
-   print("Please select a workout from the options below:")
-   # show workout options from google sheets
-   for i, workout in enumerate(workout_names, start=1):
-       print(f"{i}. {workout}")
+#Grab the workout names from the google sheet
+    workout_names = WORKOUTS.row_values(1)
+    print("Welcome to the Python Fitness Console!\n")
+    print("Please select a workout from the options below:\n")
+# show workout options from google sheets
+    for i, workout in enumerate(workout_names, start=1):
+        print(f"{i}. {workout}")
+#Obtain user disision
+user_input = int(input("Enter the number of the workout you would like to do:\n "))
+#select the workout
+if user_input == 1:
+    print(f"Great! You have selected the {BACK[user_input]} workout.")
+elif user_input == 2:
+    print(f"Great! You have selected the {ARMS[user_input]} workout.")
+elif user_input == 3:
+    print(f"Great! You have selected the {CHEST[user_input]} workout.")
+elif user_input == 4:
+    print(f"Great! You have selected the {SHOULDERS[user_input]} workout.")
+elif user_input == 5:
+    print(f"Great! You have selected the {LEGS[user_input]} workout.")
+else:
+    print("Invalid input. Please try again.")
+    
+       
+welcome()
     
 
 
