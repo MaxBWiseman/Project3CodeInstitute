35f3cde73e8871307778f3d9fa9cebdbfd9bd3a4
Max Wiseman <maxwise70@hotmail.co.uk>
2024-07-09 20:25:13 +0000

    chore: Update credentials setup to use service account file correctly

--- run.py
+++ run.py
@@ -8,8 +8,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-
-CREDS = Credentials.from_service_account_info(creds)
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("PythonFitnessConsole")
