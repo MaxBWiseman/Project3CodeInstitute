52340d149a6ff4b45dbba5436d46bde65be3de64
Max Wiseman <maxwise70@hotmail.co.uk>
2024-07-09 21:00:57 +0000

    chore: Add code to retrieve values from the "Workouts" worksheet

--- run.py
+++ run.py
@@ -12,3 +12,16 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("PythonFitnessConsole")
+
+WORKOUTS = SHEET.worksheet("Workouts")
+REPETITIONS = SHEET.worksheet("Repetitions")
+
+BACK = WORKOUTS.col_values(1)
+ARMS = WORKOUTS.col_values(2)
+CHEST = WORKOUTS.col_values(3)
+SHOULDERS = WORKOUTS.col_values(4)
+LEGS = WORKOUTS.col_values(5)
+print(value)
+
+
+
